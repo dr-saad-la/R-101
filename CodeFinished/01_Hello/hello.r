12c00ad886b2059b147e1ad38ebf4db6ee61dea1
Dr.Saad.L <56449463+dr-saad-la@users.noreply.github.com>
2024-07-20 15:40:00 +0100

    Initial Commit

---
+++ CodeFinished/01_Hello/hello.r
@@ -0,0 +1,33 @@
+# ======================================================================= 
+# Author: Dr Saad Laouadi
+# Course: R-101
+# Lesson: Hello R
+#
+# Description:
+# This is a simple R script that prints "Hello World" to the console.
+# It serves as the introductory lesson for the R-101 course, demonstrating
+# the basic syntax of R and how to run an R script from the command line.
+#
+# © Copyright: Dr. Saad Laouadi
+# ======================================================================= 
+
+# Print "Hello World" to the console
+print("Hello World")
+
+# Explanation:
+# The print() function in R is used to display text or values to the console.
+# In this example, we use print() to output the string "Hello World".
+
+# Running the Program:
+# ====================
+# To run this script, save it as hello.R and use the following command in the terminal:
+#
+# Rscript hello.R
+#
+# This command invokes the Rscript interpreter, which runs the specified R script.
+
+# Additional Information:
+# =======================
+# - R is a language and environment for statistical computing and graphics.
+# - The Rscript command allows you to execute R scripts from the command line.
+# - For more information about R, visit the official R project website: https://www.r-project.org/
---
+++ CodeStart/01_Hello/hello.r
@@ -0,0 +1,17 @@
+# ======================================================================= 
+# Author: Dr Saad Laouadi
+# Course: R-101
+# Lesson: Hello R
+#
+# Description:
+# This is a simple R script that prints "Hello World" to the console.
+# It serves as the introductory lesson for the R-101 course, demonstrating
+# the basic syntax of R and how to run an R script from the command line.
+#
+# © Copyright: Dr. Saad Laouadi
+# ======================================================================= 
+
+# Print "Hello World" to the console
+
+
+
